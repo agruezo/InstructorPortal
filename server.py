f3429ddbcd3550e85afdc50bb75d845a685ffccb
agruezo <agruezo@gmail.com>
2022-06-10 01:59:15 -0400

    created file structure

---
+++ server.py
@@ -0,0 +1,6 @@
+from flask_app import app
+# DON'T FORGET TO IMPORT ALL YOUR CONTROLLERS!!!
+from flask_app.controllers import users, ideas, lessons
+
+if __name__ == '__main__':
+    app.run(debug=True)
