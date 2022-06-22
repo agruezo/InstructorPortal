5a7493020649afafa027ec4b987eee8fa2785cf4
agruezo <agruezo@gmail.com>
2022-06-22 09:33:11 -0400

    updated server.py

--- server.py
+++ server.py
@@ -1,9 +1,6 @@
 from flask_app import app
 # DON'T FORGET TO IMPORT ALL YOUR CONTROLLERS!!!
-from flask_app.controllers import topics, users, lessons
 
-if __name__ == '__main__':
-    app.run(debug=True)
 from flask_app import app
 
 from flask_app.controllers import users, topics, lessons
